f72da531940b4b32661a5d62dd7c426095f2e549
tuyojr <olutuyod@gmail.com>
2025-12-11 12:31:55 -0600

    get alb url and ecs name

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -12,3 +12,28 @@ output "public_subnets" {
   description = "List of public subnet IDs"
   value       = module.vpc.public_subnets
 }
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.simpletimeservice_lb.dns_name
+}
+
+output "alb_url" {
+  description = "URL to access the service"
+  value       = "http://${aws_lb.simpletimeservice_lb.dns_name}"
+}
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.simpletimeservice_cluster.name
+}
+
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.ecs_service.name
+}
+
+output "cloudwatch_log_group" {
+  description = "CloudWatch Log Group for ECS tasks"
+  value       = aws_cloudwatch_log_group.ecs_task_logs.name
+}
