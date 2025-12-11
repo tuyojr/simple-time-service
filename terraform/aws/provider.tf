68342700ac3bd1ec79f6e10cd7f357d81456e2de
tuyojr <olutuyod@gmail.com>
2025-12-11 12:26:54 -0600

    added required provider versions

---
+++ terraform/aws/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket = "particle41-backend"
+    key = "terraform/state"
+    region = "us-east-1"
+    dynamodb_table = "particle41-lock-table"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
+}
